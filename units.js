a2ebfbdcf3532a39368a9b9d71a6f2dadbb3ad02
Michael Weeks <Michael.Weeks@MobomoLLC.onmicrosoft.com>
2022-08-28 14:24:39 -0600

    add damage table, now we know unit advantages

--- units.js
+++ units.js
@@ -197,6 +197,35 @@ function findTriplets(center) {
   return trips;
 };
 
+
+// The damage table.  If the combination is not found in here, some one is cheating.
+function calcDmg(unit1, unit2) {
+  // Calculated in google sheets
+  // advantage  =min(I4,M4)*3 + min(J4,N4)*3 + min(K4,L4)*3
+  // defense  =min(N4*I4,1)+min(L4*J4,1)+min(M4*K4,1)
+  // damage = max(advantage - defense, 0) ... +1 on pure elementals against self or perfect advantage
+  var table = {
+    'h3f0m0': {'h3f0m0':1, 'h0f3m0':10, 'h0f0m3':0, 'h2f1m0':3, 'h2f0m1':1, 'h1f2m0':6, 'h0f2m1':5, 'h1f0m2':1, 'h0f1m2':2, 'h1f1m1':2}, // sum 34
+    'h0f3m0': {'h3f0m0':0, 'h0f3m0':1, 'h0f0m3':10, 'h2f1m0':1, 'h2f0m1':2, 'h1f2m0':1, 'h0f2m1':3, 'h1f0m2':5, 'h0f1m2':6, 'h1f1m1':2}, // sum 34
+    'h0f0m3': {'h3f0m0':10, 'h0f3m0':0, 'h0f0m3':1, 'h2f1m0':5, 'h2f0m1':6, 'h1f2m0':2, 'h0f2m1':1, 'h1f0m2':3, 'h0f1m2':1, 'h1f1m1':2}, // sum 34
+    'h2f1m0': {'h3f0m0':0, 'h0f3m0':6, 'h0f0m3':2, 'h2f1m0':2, 'h2f0m1':1, 'h1f2m0':5, 'h0f2m1':8, 'h1f0m2':1, 'h0f1m2':5, 'h1f1m1':4}, // sum 34
+    'h2f0m1': {'h3f0m0':3, 'h0f3m0':5, 'h0f0m3':0, 'h2f1m0':5, 'h2f0m1':2, 'h1f2m0':8, 'h0f2m1':4, 'h1f0m2':2, 'h0f1m2':1, 'h1f1m1':4}, // sum 30
+    'h1f2m0': {'h3f0m0':0, 'h0f3m0':3, 'h0f0m3':5, 'h2f1m0':2, 'h2f0m1':1, 'h1f2m0':2, 'h0f2m1':5, 'h1f0m2':4, 'h0f1m2':8, 'h1f1m1':4}, // sum 30
+    'h0f2m1': {'h3f0m0':2, 'h0f3m0':0, 'h0f0m3':6, 'h2f1m0':1, 'h2f0m1':5, 'h1f2m0':1, 'h0f2m1':2, 'h1f0m2':8, 'h0f1m2':5, 'h1f1m1':4}, // sum 34
+    'h1f0m2': {'h3f0m0':6, 'h0f3m0':2, 'h0f0m3':0, 'h2f1m0':8, 'h2f0m1':5, 'h1f2m0':5, 'h0f2m1':1, 'h1f0m2':2, 'h0f1m2':1, 'h1f1m1':4}, // sum 34
+    'h0f1m2': {'h3f0m0':5, 'h0f3m0':0, 'h0f0m3':3, 'h2f1m0':4, 'h2f0m1':8, 'h1f2m0':1, 'h0f2m1':2, 'h1f0m2':5, 'h0f1m2':2, 'h1f1m1':4}, // sum 30
+    'h1f1m1': {'h3f0m0':2, 'h0f3m0':2, 'h0f0m3':2, 'h2f1m0':4, 'h2f0m1':4, 'h1f2m0':4, 'h0f2m1':4, 'h1f0m2':4, 'h0f1m2':4, 'h1f1m1':6}, // sum 36
+  };
+  // magic -> hard -> fast -> magic... and 2magic-1hard gives more damage potential than 2hard-1magic
+  // strong > weak gives you 34, weak > strong gives you 30
+  if( $(table).hasOwnProperty(unit1) && $(table[unit1]).hasOwnProperty(unit2) ) {
+    return table[unit1][unit2];
+  }
+  // So table[unit1][unit2] is undefined?  Something hinky going on.
+  return 0;
+  // You cheat?  You do no damage.  The battle will never end and you will stop the cheating.
+}
+
 // listeners
 
 // ToDo: make a unit that is controllable by the user 
